13d36719fe16ec3c250a52827f3ddda250cc4f9d
pbobinsk <62461206+pbobinski-WSB@users.noreply.github.com>
2025-12-11 15:12:30 +0100

    Wykład siódmy - atm, invoice and SOLID

---
+++ src/wsb/po/siedem/invoice/LineItem.java
@@ -0,0 +1,42 @@
+package wsb.po.siedem.invoice;
+
+/**
+   Describes a quantity of an article to purchase.
+*/
+public class LineItem
+{  
+   private int quantity;
+   private Product theProduct;
+
+   /**
+      Constructs an item from the product and quantity.
+      @param product the product
+      @param quantity the item quantity
+   */
+   public LineItem(Product product, int quantity)
+   {  
+      theProduct = product;
+      this.quantity = quantity;
+   }
+   
+   /**
+      Computes the total cost of this line item.
+      @return the total price
+   */
+   public double getTotalPrice()
+   {
+
+      return theProduct.getPrice() * quantity;
+   }
+   
+   /**
+      Formats this item.
+      @return a formatted string of this item
+   */
+   public String format()
+   {
+      return String.format("%-30s%8.2f%5d%8.2f",
+              theProduct.getDescription(), theProduct.getPrice(),
+              quantity, getTotalPrice());
+   }
+}
---
+++ src/wsb/po/siedem/invoice/Product.java
@@ -0,0 +1,45 @@
+package wsb.po.siedem.invoice;
+
+/**
+   Describes a product with a description and a price.
+*/
+public class Product
+{  
+   private String description;
+   private double price;
+
+   /**
+      Constructs a product from a description and a price.
+      @param  description product description
+      @param price the product price
+   */
+   public Product(String description, double price) throws ArithmeticException
+   {
+      if (price < 0) {
+         throw new ArithmeticException("Price cannot be negative");
+      }
+      this.description = description;
+      this.price = price;
+   }
+   
+   /**
+      Gets the product description.
+      @return the description
+   */
+   public String getDescription()
+   {  
+
+      return description;
+   }
+
+   /**
+      Gets the product price.
+      @return the unit price
+   */
+   public double getPrice()
+   {
+
+      return price;
+   }
+}
+
